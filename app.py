820bdb4efb5b93a51a332a8a48fa81167a3ea9ae
Chamod Shehanka Perera <hcsperera@outlook.com>
2019-06-16 00:35:24 +0530

    Update

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ from twilio.rest import Client
 client = Client()
 
 from_whatsapp_number='whatsapp:+'
-to_whatsapp_number='whatsapp:' + os.environ['MY_PHONE']
+to_whatsapp_number='whatsapp:' + os.environ['MY_PHONE_NUMBER']
+
+client.messages.create(body='Hello Twilio!',
+                        from_=from_whatsapp_number,
+                        to=to_whatsapp_number)
