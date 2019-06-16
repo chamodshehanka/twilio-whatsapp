d5d2a2b649f5e932c95161c30c7c5dbb1ab366ec
Chamod Shehanka Perera <hcsperera@outlook.com>
2019-06-16 11:41:02 +0530

    Phone numbers added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 
 client = Client()
 
-from_whatsapp_number='whatsapp:+'
+from_whatsapp_number='whatsapp:+14155238886'
 to_whatsapp_number='whatsapp:' + os.environ['MY_PHONE_NUMBER']
 
 client.messages.create(body='Hello Twilio!',
